2d2323294fce759376ed407a716104193729cb4f
Sahil Gohil <gohilsammy@gmail.com>
2026-03-05 08:44:36 +0000

    fix: theme provider build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: ["https://6000-idx-studio-1746005081349.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"],
+    // allowedDevOrigins: ["https://6000-idx-studio-1746005081349.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev"],
   },
 };
 
